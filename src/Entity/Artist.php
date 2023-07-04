117fc01603c48e93ef23fbf885006c020455afdc
Bahaaeddine <bahaaeddine.ghezz@gmail.com>
2023-07-04 11:16:18 +0200

    second Projet

---
+++ src/Entity/Artist.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArtistRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArtistRepository::class)]
+class Artist
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $first_name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $last_name = null;
+
+    #[ORM\ManyToMany(targetEntity: Music::class, inversedBy: 'artists')]
+    private Collection $musics;
+
+    public function __construct()
+    {
+        $this->musics = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(string $first_name): static
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->last_name;
+    }
+
+    public function setLastName(string $last_name): static
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Music>
+     */
+    public function getMusics(): Collection
+    {
+        return $this->musics;
+    }
+
+    public function addMusic(Music $music): static
+    {
+        if (!$this->musics->contains($music)) {
+            $this->musics->add($music);
+        }
+
+        return $this;
+    }
+
+    public function removeMusic(Music $music): static
+    {
+        $this->musics->removeElement($music);
+
+        return $this;
+    }
+}
